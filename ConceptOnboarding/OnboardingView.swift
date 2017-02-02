23fed41e22977eb62e8f2e24752c5e1ad3ad0d94
Lovish Dogra <lovishdogra7@gmail.com>
2017-02-02 19:38:24 +0530

    Resources

---
+++ ConceptOnboarding/OnboardingView.swift
@@ -0,0 +1,22 @@
+//
+//  OnboardingView.swift
+//  ConceptOnboarding
+//
+//  Created by Lovish Dogra on 01/02/17.
+//  Copyright © 2017 Lovish Dogra. All rights reserved.
+//
+
+import UIKit
+import paper_onboarding
+
+class OnboardingView: PaperOnboarding {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
